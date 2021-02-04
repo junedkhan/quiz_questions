2edb0c71de72e6a80cf3d724e701cd94cfd7bb51
junedkhan <junedkhan933@gmail.com>
2021-02-04 23:36:34 +0530

    added routes and data

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const { questionData, answers } = require('./questionsData');
+
 const app = express();
+app.use(express.json())
 
 let port = process.env.PORT || 3000;
 
 app.get('/questions', (req, res) => {
-    res.send({
-        "name": "Juned khan",
-    });
-})
+    res.send(questionData);
+});
+
+app.post('/check', (req, res) => {
+    const userAnswers = req.body.answers;
+    const result = {
+        correct: 0,
+        incorrect: 0
+    };
+    for(ans in userAnswers) {
+        console.log("ans", ans, "val", userAnswers[ans])
+        if(userAnswers[ans] === answers[ans]) {
+            result.correct  += 1;
+        } else {
+            result.incorrect += 1;
+        }
+    }
+    res.send(result);
+});
 
 app.listen(port, () => {
     console.log(`App is running in port ${port}`);
---
+++ questionsData.js
@@ -0,0 +1,38 @@
+const questionData = [{
+    "id": 1,
+    "question": "what is capital of India?",
+    "options": "['Kolkata', 'Banglore', 'Delhi', 'Gujrat']",
+},
+{
+    "id": 2,
+    "question": "what is capital of India?",
+    "options": "['Kolkata', 'Banglore', 'Delhi', 'Gujrat']",
+},
+{
+    "id": 3,
+    "question": "what is capital of India?",
+    "options": "['Kolkata', 'Banglore', 'Delhi', 'Gujrat']",
+},
+{
+    "id": 4,
+    "question": "what is capital of India?",
+    "options": "['Kolkata', 'Banglore', 'Delhi', 'Gujrat']",
+},
+{
+    "id": 5,
+    "question": "what is capital of India?",
+    "options": "['Kolkata', 'Banglore', 'Delhi', 'Gujrat']",
+}];
+
+const answers = {
+    "1": "Delhi",
+    "2": "Delhi",
+    "3": "Delhi",
+    "4": "Delhi",
+    "5": "Delhi",
+}
+
+module.exports = {
+    questionData,
+    answers,
+}
