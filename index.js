62abf81baf96a22ac3f328a3edefbc04cc912829
junedkhan <junedkhan933@gmail.com>
2021-02-04 22:02:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+let port = process.env.PORT || 3000;
+
+app.get('/questions', (req, res) => {
+    res.send({
+        "name": "Juned",
+    });
+})
+
+app.listen(port, () => {
+    console.log(`App is running in port ${port}`);
+});
