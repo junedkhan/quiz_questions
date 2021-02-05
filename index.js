66efde958ffe644070437d461c0c59a3aa988ad3
junedkhan <junedkhan933@gmail.com>
2021-02-05 18:50:53 +0530

    added errors object

--- index.js
+++ index.js
@@ -18,16 +18,21 @@ app.post('/check', (req, res) => {
     const userAnswers = req.body.answers;
     const result = {
         correct: 0,
-        incorrect: 0
+        incorrect: 0,
+        errors: {}
     };
+
+    // Making response with data errors, and correct, incorrect fields
     for(ans in userAnswers) {
-        console.log("ans", ans, "val", userAnswers[ans])
         if(userAnswers[ans] === answers[ans]) {
             result.correct  += 1;
+            result.errors[ans] = false;
         } else {
             result.incorrect += 1;
+            result.errors[ans] = true;
         }
     }
+
     res.send(result);
 });
 
