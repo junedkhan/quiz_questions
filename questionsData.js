acbd5448d29cc31a3f4036783baab5f3a6b13fad
junedkhan <junedkhan933@gmail.com>
2021-02-05 18:00:17 +0530

    corrected data

--- questionsData.js
+++ questionsData.js
@@ -1,27 +1,27 @@
 const questionData = [{
     "id": 1,
     "label": "what is capital of India?",
-    "options": "['Kolkata', 'Banglore', 'Delhi', 'Gujrat']",
+    "options": ['Kolkata', 'Banglore', 'Delhi', 'Gujrat'],
   },
   {
     "id": 2,
     "label": "Which of the following is not JavaScript Data Types?",
-    "options": "['Undefined', 'Number', 'Boolean', 'Float']",
+    "options": ['Undefined', 'Number', 'Boolean', 'Float'],
   },
   {
     "id": 3,
     "label": "Which company developed JavaScript?",
-    "options": "['Netscape', 'Bell Labs', 'Sun Microsystems', 'IBM']",
+    "options": ['Netscape', 'Bell Labs', 'Sun Microsystems', 'IBM'],
   },
   {
     "id": 4,
     "label": "What is the original name of JavaScript?",
-    "options": "['LiveScript', 'EScript', 'Mocha', 'JavaScript']",
+    "options": ['LiveScript', 'EScript', 'Mocha', 'JavaScript'],
   },
   {
     "id": 5,
     "label": "Among the keywords below, which one is not a statement?",
-    "options": "['if', 'with', 'debugger', 'use strict']",
+    "options": ['if', 'with', 'debugger', 'use strict'],
   }];;
 
 const answers = {
